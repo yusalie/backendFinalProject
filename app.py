6205521a9227b9e048006d6f73b30653c6682e55
Yusuf <yusuf.salie96@mail.com>
2021-02-24 15:31:22 +0200

    update for backend

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import jsonify, Flask
+import sqlite3
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+def dict_factory(cursor, row):
+    d={}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]]=row[idx]
+    return d
+
+@app.route('/')
+def show_data():
+    try:
+        with sqlite3.connect('database.db') as con:
+            con.row_factory = dict_factory
+            cursor = con.cursor()
+            cursor.execute('SELECT * FROM manga')
+            data = cursor.fetchall()
+            print(data)
+            print(jsonify(data))
+            return jsonify(data)
+    except:
+        pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
